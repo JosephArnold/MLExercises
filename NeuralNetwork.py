857299f2bb40240fa599e89f0e91a721e51d9e0d
Joseph Arnold Xavier <xjoseph.arnold@gmail.com>
2023-05-23 12:57:42 +0200

    Simple Neural network using BP and Adam optimizer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import random
+
 
 # load the dataset, split into input (X) and output (y) variables
 dataset = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
@@ -48,38 +50,42 @@ class PimaClassifier(nn.Module):
         return x
 
 model = PimaClassifier()
-print(model)
+#print(model)
+
+n_input_layer = 8
+n_layer1 = 12
+n_layer2 = 8
+n_output_layer = 1
 
 loss_fn = nn.BCELoss()  # binary cross entropy
-#optimizer = optim.Adam(model.parameters(), lr=0.001)
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+#optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-n_epochs = 1
-batch_size = 1
+n_epochs = 200
+batch_size = 10
 
-#To print the weights
-#https://stackoverflow.com/questions/67722328/how-to-check-the-output-gradient-by-each-layer-in-pytorch-in-my-code
 for epoch in range(n_epochs):
     for i in range(0, len(X), batch_size):
         Xbatch = X[i:i+batch_size]
-        y_pred = model(Xbatch)
         ybatch = y[i:i+batch_size]
-        loss = loss_fn(y_pred, ybatch)
+        y_pred = model(Xbatch)
         optimizer.zero_grad()
+        loss = loss_fn(y_pred, ybatch)
         loss.backward()
-        
         optimizer.step()
 
     print(f'Finished epoch {epoch}, latest loss {loss}')
-
-
+    
 # compute accuracy (no_grad is optional)
+
 with torch.no_grad():
+
     y_pred = model(X)
-accuracy = (y_pred.round() == y).float().mean()
-print(f"Accuracy {accuracy}")
+    accuracy = (y_pred.round() == y).float().mean()
+    print(f"Accuracy {accuracy}")
 
 # make class predictions with the model
 predictions = (model(X) > 0.5).int()
 for i in range(5):
     print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+
