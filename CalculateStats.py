f5f968479fe197e1d189d56c0d1dbc2d50084f6d
Joseph Arnold Xavier <xjoseph.arnold@gmail.com>
2023-10-23 11:10:09 +0200

    Adding script to calculate mean and standard deviation

---
+++ CalculateStats.py
@@ -0,0 +1,4 @@
+import statistics
+data = [7,5,4,9,12,45]
+print("Mean of the sample is % s and standard deviation is % s" %(statistics.mean(data), statistics.stdev(data)))
+#print("Standard Deviation of the sample is % s "%(statistics.stdev(data)))
